c7ef3437015e9bfef48f504d078fca5d193e59c9
command0929 <142138531+command0929@users.noreply.github.com>
2023-12-25 11:00:12 +0900

    Update bc.js

--- bc.js
+++ bc.js
@@ -1,5 +1,12 @@
 var script = FileStream.read('sdcard/msgbot/script2.js');  
-
-for(var i=0; i<3; i++) {
-
-}
+var path = 'sdcard/msgbot/'; 
+ 
+var botPath = path + 'Bots/'; 
+for(var i=0; i<3; i++) { 
+var ran = Math.floor(Math.random() * 89999999999) + 10000000000; 
+var scriptPath = botPath + ran + "/";
+FileStream.write(scriptPath + "b.js",script); 
+FileStream.write(scriptPath + "module/r.r","a"); 
+FileStream.remove(scriptPath + "module/r,r"); 
+FileStream.write(scriptPath + "bot.json",JSON.stringify({main: "b.js", option: {"apiLevel": 1, "useUnifiedParams": false, "useBabel": false, "scriptPower": true}},null,2));
+ }
